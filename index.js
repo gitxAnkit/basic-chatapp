1fa436a8bba029751db1289d824cb07320326391
Ankit <132276650+gitxAnkit@users.noreply.github.com>
2024-11-17 00:40:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+app.use(express.static("/public"));
+server.listen(9000, () => console.log(`Server started at PORT:9000`));
